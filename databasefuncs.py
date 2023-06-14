4438755a3386120503443a415e628b56f8ffccbe
TarikBurhan <tarikburhan1@gmail.com>
2023-06-15 00:35:44 +0300

    register, user login and user deletion
    User registration, user login and user deletion functions to databasefuncs.py implemented. Registration post method, user login get method and user deletion delete method implemented.

--- databasefuncs.py
+++ databasefuncs.py
@@ -1,8 +1,17 @@
 import sqlite3
 import pandas as pd
+from hashlib import sha256 as hasher
+import base64
+
+def create_hash(password):
+    pw_bytestring = password.encode()
+    return hasher(pw_bytestring).hexdigest()
 
 
 class MySqliteDatabase:
+    """
+    SQLite3 EsportsEarnings database functions.
+    """
     def __init__(self):
         self.conn = sqlite3.connect('ESportsEarnings.db')
         cursor = self.conn.cursor()
@@ -10,6 +19,9 @@ class MySqliteDatabase:
         cursor.close()
 
     def getTableNames(self) -> dict:
+        """
+        Returns table names of EsportsEarnings database.
+        """
         cur = self.conn.cursor()
         cur.execute(f"SELECT name FROM sqlite_master WHERE type='table'")
         tables = cur.fetchall()
@@ -22,6 +34,11 @@ class MySqliteDatabase:
         return {'Table Names': table_names, 'Table Count': len(table_names)}
 
     def getColumnNames(self, table_name:str) -> dict:
+        """
+        :param table_name: Table name in database.
+
+        Return column names of given table name in EsportsEarnings database.
+        """
         cur = self.conn.cursor()
         cur.execute(f'SELECT * FROM {table_name}')
         columns = list(map(lambda x: x[0], cur.description))
@@ -30,22 +47,17 @@ class MySqliteDatabase:
         cur.close()
         return {'Column Names': columns, 'Column Count': len(columns), 'Total Rows': count[0][0]}
     
-    def getAll(self, table_name, column=None, order=None) -> list:
+    def getAll(self, table_name) -> list:
         """
-        If column is not specified, order has no usage.\n
-        order is either ASC or DESC
+        :param table_name: Table name in database.
+
+        Return all table contents to given table name in EsportsEarnings database.
         """
         cur = self.conn.cursor()
 
         columns = self.getColumnNames(table_name)['Column Names']
 
-        if column in columns and order != None:
-            if order == 'ASC':
-                cur.execute(f'SELECT * FROM {table_name} ORDER BY {column} ASC')
-            elif order == 'DESC':
-                cur.execute(f'SELECT * FROM {table_name} ORDER BY {column} DESC')
-        else:
-            cur.execute(f'SELECT * FROM {table_name}')
+        cur.execute(f'SELECT * FROM {table_name}')
         table = cur.fetchall()
         cur.close()
         table_content = []
@@ -53,8 +65,13 @@ class MySqliteDatabase:
             table_content.append(dict(zip(columns, list)))
         return table_content
     
-
     def search(self, table_name:str, columns:list) -> list:
+        """
+        :param table_name: Table name in database.
+        :param columns: Column names given table name.
+
+        Return given columns contents to given table name in EsportsEarnings database.
+        """
         if columns:
             table_columns = self.getColumnNames(table_name)['Column Names']
             for column in columns:
@@ -78,7 +95,72 @@ class MySqliteDatabase:
             table_content.append(dict(zip(columns, list)))
         return table_content
 
+    def register(self, name:str, surname:str, email:str, password:str, profilepic:bytes, 
+                 nationality=None, birthDate=None, gender=None, description=None) -> bool:
+        """
+        Return booelan value corresponding to registration of user with given parameters.
+        """
+        cur = self.conn.cursor()
+        userExists = cur.execute("SELECT count(email) FROM LoginTable WHERE email=?", (email, )).fetchone()[0] != 0
+        if userExists:
+            cur.close()
+            return False
+        else:
+            cur.execute("""INSERT INTO LoginTable (name, surname, birthDate, gender, nationality, 
+                                email, password, isAdmin, profilePic, userDesc) 
+                                values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", 
+                                (name, surname, birthDate, gender, nationality, email, 
+                                 create_hash(password), 0, profilepic, description))
+            cur.fetchall()
+            self.conn.commit()
+            cur.close()
+            return True
 
+    def login(self, email:str, password:str) -> int:
+        """
+        :param email: E-Mail address
+        :param password: Password 
+
+        Returns an integer, 0: User is not registered, 1: Login Successful, 2: Incorrect password
+        """
+        cur = self.conn.cursor()
+        userExists = cur.execute("SELECT count(email) FROM LoginTable WHERE email=?", (email, )).fetchone()[0] != 0
+        if not userExists:
+            return 0 
+        
+        hash_pw = create_hash(password)
+        
+        user_pw = cur.execute("SELECT password FROM LoginTable WHERE email=?", (email, )).fetchone()[0]
+        if hash_pw != user_pw:
+            return 2
+        cur.close()
+        return 1
+
+    def delete(self, email:str, password:str) -> int:
+        """
+        :param email: E-Mail address
+        :param password: Password 
+
+        Returns an integer, 0: User is not found, 1: Deletion Successful, 2: Incorrect password
+        """
+        cur = self.conn.cursor()
+        userExists = cur.execute("SELECT count(email) FROM LoginTable WHERE email=?", (email, )).fetchone()[0] != 0
+        if not userExists:
+            return 0 
+        
+        hash_pw = create_hash(password)
+        
+        user_pw = cur.execute("SELECT password FROM LoginTable WHERE email=?", (email, )).fetchone()[0]
+        if hash_pw != user_pw:
+            return 2
+        cur.execute("DELETE FROM LoginTable WHERE email=?", (email, ))
+        self.conn.commit()
+        cur.close()
+        return 1
+    
+    def post(self, email:str, password:str, arguments:dict) -> int:
+        
+        pass
 
 
     """def kwargsexmple(self, table_name, **kwargs):
--- myapi.py
+++ myapi.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
 from databasefuncs import MySqliteDatabase
-
+import werkzeug
 
 
 app = Flask(__name__)
@@ -10,6 +10,28 @@ api = Api(app)
 table_content_args = reqparse.RequestParser()
 table_content_args.add_argument('columns', type=str)
 
+register_args = reqparse.RequestParser()
+register_args.add_argument('name', type=str, location='form', required=True, help='Name has to be given (name)')
+register_args.add_argument('surname', type=str, location='form', required=True, help='Surname has to be given (surname)')
+register_args.add_argument('birthDate', type=str, location='form', required=False)
+register_args.add_argument('gender', type=str, location='form', required=False)
+register_args.add_argument('nationality', type=str, location='form', required=False)
+register_args.add_argument('email', type=str, location='form', required=True, help='E-Mail address has to be given (email)')
+register_args.add_argument('password', type=str, location='form', required=True, help='Password has to be given (password)')
+register_args.add_argument('profilepic', type=werkzeug.datastructures.FileStorage, location='files', required=False)
+register_args.add_argument('userDesc', type=str, location='form', required=False)
+
+user_args = reqparse.RequestParser()
+user_args.add_argument('name', type=str, location='form', required=False, help='Name has to be given (name)')
+user_args.add_argument('surname', type=str, location='form', required=False, help='Surname has to be given (surname)')
+user_args.add_argument('birthDate', type=str, location='form', required=False)
+user_args.add_argument('gender', type=str, location='form', required=False)
+user_args.add_argument('nationality', type=str, location='form', required=False)
+user_args.add_argument('email', type=str, location='form', required=True, help='E-Mail address has to be given (email)')
+user_args.add_argument('password', type=str, location='form', required=True, help='Password has to be given (password)')
+user_args.add_argument('profilepic', type=werkzeug.datastructures.FileStorage, location='files', required=False)
+user_args.add_argument('userDesc', type=str, location='form', required=False)
+user_args.add_argument('newpassword', type=str, location='form', required=False)
 
 class TableNames(Resource):
     def get(self):
@@ -38,10 +60,48 @@ class TableContentsAll(Resource):
             return abort(400, description='This table does not exists. For more information look /tableinfo')
 
 
+class Register(Resource):
+    def post(self):
+        args = register_args.parse_args()
+        myDB = MySqliteDatabase()
+        result = myDB.register(name=args['name'], surname=args['surname'], email=args['email'], password=args['password'],
+                                profilepic=args['profilepic'].read())
+        return ({"Result": f"{args['email']} has been registered."}, 201) if result else ({"Result": "This user already exists."}, 409)
+
+
+class User(Resource):
+    def get(self):
+        args = user_args.parse_args()
+        myDB = MySqliteDatabase()
+        result = myDB.login(args['email'], args['password'])
+        if result == 0:
+            return ({"Result": 'This user is not registered.'}, 404)
+        elif result == 1:
+            return ({"Result": 'Successful login.'}, 200)
+        elif result == 2:
+            return ({"Result": 'Wrong password.'}, 401)
+    
+    def delete(self):
+        args = user_args.parse_args()
+        myDB = MySqliteDatabase()
+        result = myDB.delete(args['email'], args['password'])
+        print(result)
+        if result == 0:
+            return ({"Result": "This account does not exist."}, 404)
+        elif result == 1:
+            return ({"Result": "Successfully deleted the account."}, 200)
+        else:
+            return ({"Result": "Wrong password, account has not been deleted."}, 401)
+
+    def post(self):
+        args = user_args.parse_args()
+        
+
 api.add_resource(TableNames, '/tableinfo')
 api.add_resource(TableColumnNames, '/tableinfo/<string:table_name>')
 api.add_resource(TableContentsAll, '/contents/<string:table_name>')
-
+api.add_resource(Register, '/register')
+api.add_resource(User, '/user')
 
 if __name__ == '__main__':
     app.run(debug=True)
